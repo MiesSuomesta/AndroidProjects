a53eda1785134858d93806dde248f47a1df940af
lja <lja@iki.fi>
2019-08-02 13:37:39 +0300

    first 3d object visible
    Signed-off-by: lja <lja@iki.fi>

---
+++ 3dlja/app/src/main/java/com/lja/a3dlja/MyGLRenderer.java
@@ -0,0 +1,57 @@
+package com.lja.a3dlja;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+    private Triangle mTriangle;
+    private Square   mSquare;
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
+
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        // Set the background frame color
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        // initialize a triangle
+        mTriangle = new Triangle(
+                0.0f,  0.622008459f, 0.0f, // top
+                -0.5f, -0.311004243f, 0.0f, // bottom left
+                0.5f, -0.311004243f, 0.0f,  // bottom right
+                0.5f,0,0,1);
+
+        // initialize a square
+        // initialize a square
+        mSquare = new Square(-0.5f,  0.5f, 0.0f,   // top left
+                -0.5f, -0.5f, 0.0f,   // bottom left
+                0.5f, -0.5f, 0.0f,   // bottom right
+                0.5f,  0.5f, 0.0f,    // top right
+                0.2f,0,0,1);
+    }
+
+    public void onDrawFrame(GL10 unused) {
+        // Redraw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        mTriangle.draw();
+        mSquare.draw();
+    }
+
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+}
